89cd16c720ad708a995978daf5ce104d4822f8cd
strykershy <61974860+strykershy@users.noreply.github.com>
2020-12-11 12:03:12 +0000

    4th Commit

--- newsx/newsapp/urls.py
+++ newsx/newsapp/urls.py
@@ -2,6 +2,10 @@
 from django.conf.urls import url
 from . import views
 
+
+app_name = 'articles'
+
 urlpatterns = [
-    url(r'^$', views.newsitems),
+    url(r'^$', views.newsitems, name = "items" ),
+    url(r'^(?P<slug>[\w-]+)/$', views.details, name = "detail" )
 ]
--- newsx/newsapp/views.py
+++ newsx/newsapp/views.py
@@ -1,6 +1,11 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 def newsitems(request):
     articles = Article.objects.all().order_by('date_published')
     return render(request, 'newsapp/newsitems.html', {'articles': articles})
+
+def details(request, slug):
+    return HttpResponse(slug)
